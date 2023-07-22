79a6d38f9d27e4371b3365c141bd84594c5ebfab
rajfly <rajdeep@outlook.sg>
2023-07-22 17:28:35 +0000

    added kernel_init

--- train.py
+++ train.py
@@ -6,19 +6,19 @@ def residual_block(x, filters, projection):
 
     # layer 1
     if projection:
-        x = tf.keras.layers.Conv2D(filters, (3, 3), (2,2), padding='same')(x)
+        x = tf.keras.layers.Conv2D(filters, (3, 3), (2,2), padding='same', kernel_initializer='he_normal')(x)
     else:
-        x = tf.keras.layers.Conv2D(filters, (3, 3), (1,1), padding='same')(x)
+        x = tf.keras.layers.Conv2D(filters, (3, 3), (1,1), padding='same', kernel_initializer='he_normal')(x)
     x = tf.keras.layers.BatchNormalization(axis=3)(x)
     x = tf.keras.layers.Activation('relu')(x)
 
     # layer 2
-    x = tf.keras.layers.Conv2D(filters, (3, 3), (1,1), padding='same')(x)
+    x = tf.keras.layers.Conv2D(filters, (3, 3), (1,1), padding='same', kernel_initializer='he_normal')(x)
     x = tf.keras.layers.BatchNormalization(axis=3)(x)
 
     # addition
     if projection:
-        x_skip = tf.keras.layers.Conv2D(filters, (1, 1), (2,2), padding='valid')(x_skip)
+        x_skip = tf.keras.layers.Conv2D(filters, (1, 1), (2,2), padding='valid', kernel_initializer='he_normal')(x_skip)
     x = tf.keras.layers.Add()([x, x_skip])
     x = tf.keras.layers.Activation('relu')(x)
 
