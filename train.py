ded89d2e6695a37aeb8b0fff7e97f3c08fee0290
rajfly <rajdeep@outlook.sg>
2023-07-22 18:26:40 +0000

    left resnet20 first conv and maxpool

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
+# standard resnet block
 def residual_block(x, filters, projection):
     x_skip = x 
 
@@ -25,9 +26,16 @@ def residual_block(x, filters, projection):
     return x
 
 # configured for resnet-20
-def residual_stack(x):
-    filters = [16, 32, 64]
+# only left first conv and max pool
+def resnet20(shape_in, classes):
+    # input and initial convolution
+    x_in = tf.keras.layers.Input(shape_in)
+    x = tf.keras.layers.Conv2D(16, (3, 3), (1,1), padding='same', kernel_initializer='he_normal')(x_in)
+    x = tf.keras.layers.BatchNormalization(axis=3)(x)
+    x = tf.keras.layers.Activation('relu')(x)
 
+    # residual blocks
+    filters = [16, 32, 64]
     for i in range(3):
         for j in range(3):
             if i > 0 and j == 0:
@@ -35,7 +43,13 @@ def residual_stack(x):
             else:
                 x = residual_block(x, filters[i], projection=False)
 
-    return x
+    # final dense layer and model 
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Flatten()(x)
+    x_out = tf.keras.layers.Dense(classes, kernel_initializer='he_normal')(x)
+    model = tf.keras.models.Model(x_in, x_out, name='ResNet20')
+
+    return model
 
 if __name__ == "__main__":
     # load dataset
@@ -43,4 +57,11 @@ if __name__ == "__main__":
     keys = list(dataset.keys())
     for key in keys:
         print(key, dataset[key].shape)
+    
+    # compile model
+    model = resnet20((28,28,3), 9)
+    optim = tf.keras.optimizers.Adam(lr=0.001)
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    model.compile(optimizer=optim, loss=loss, metrics=['accuracy'])
+    model.summary()
     
