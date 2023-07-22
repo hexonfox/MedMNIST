0bb1824559e082ac070f3ef8267b33e9092bf8cf
rajfly <rajdeep@outlook.sg>
2023-07-22 14:37:04 +0000

    minor

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 def residual_block(x, filters, projection=False):
     x_skip = x 
 
+    # layer 1
     if projection:
         x = tf.keras.layers.Conv2D(filters, (3, 3), (2,2), padding='same')(x)
     else:
@@ -12,14 +13,16 @@ def residual_block(x, filters, projection=False):
     x = tf.keras.layers.BatchNormalization(axis=3)(x)
     x = tf.keras.layers.Activation('relu')(x)
 
+    # layer 2
     x = tf.keras.layers.Conv2D(filters, (3, 3), (1,1), padding='same')(x)
     x = tf.keras.layers.BatchNormalization(axis=3)(x)
 
+    # addition
     if projection:
         x_skip = tf.keras.layers.Conv2D(filters, (1, 1), (2,2), padding='valid')(x_skip)
-
     x = tf.keras.layers.Add()([x, x_skip])
     x = tf.keras.layers.Activation('relu')(x)
+
     return x
 
 if __name__ == "__main__":
