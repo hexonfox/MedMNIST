1b167cf0bcc101dce411ea274063a6b5154bc656
rajfly <rajdeep@outlook.sg>
2023-07-23 14:59:07 +0000

    add convnet

--- train.py
+++ train.py
@@ -96,7 +96,28 @@ if __name__ == "__main__":
             ])
             dataset[i] = tf.keras.applications.vgg16.preprocess_input(dataset[i])
     elif args.model == 'convnet': 
-        pass
+        model = tf.keras.Sequential([
+            tf.keras.layers.Input((28,28,3)),
+             
+            tf.keras.layers.Conv2D(64, (3,3), (1,1), padding='valid', kernel_initializer='he_normal', activation='relu'),
+            tf.keras.layers.Conv2D(64, (3,3), (1,1), padding='valid', kernel_initializer='he_normal', activation='relu'),
+            tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+            tf.keras.layers.Dropout(0.25),
+
+            tf.keras.layers.Conv2D(128, (3,3), (1,1), padding='valid', kernel_initializer='he_normal', activation='relu'),
+            tf.keras.layers.Conv2D(128, (3,3), (1,1), padding='valid', kernel_initializer='he_normal', activation='relu'),
+            tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+            tf.keras.layers.Dropout(0.25),
+
+            tf.keras.layers.Conv2D(256, (3,3), (1,1), padding='valid', kernel_initializer='he_normal', activation='relu'),
+            tf.keras.layers.Conv2D(256, (3,3), (1,1), padding='valid', kernel_initializer='he_normal', activation='relu'),
+            tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+            tf.keras.layers.Dropout(0.25),
+
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(512, kernel_initializer='he_normal', activation='relu'),
+            tf.keras.layers.Dense(9, kernel_initializer='he_normal', activation=None),
+        ])
     optim = tf.keras.optimizers.Adam(lr=0.001)
     loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     model.compile(optimizer=optim, loss=loss, metrics=['accuracy'])
