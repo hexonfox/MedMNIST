a2a52cb520a4b5235b34411a59d8b8500c688f45
rajdeephu <rajdeephu@gmail.com>
2023-09-19 15:07:24 +0700

    minor

--- train.sh
+++ train.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-python train.py --gpu 1 --model resnet20 --dataset pathmnist
-python train.py --gpu 1 --model vgg16 --dataset pathmnist
-python train.py --gpu 1 --model convnet --dataset pathmnist
+python train.py --gpu 0 --model resnet20 --dataset pathmnist
+python train.py --gpu 0 --model vgg16 --dataset pathmnist
+python train.py --gpu 0 --model convnet --dataset pathmnist
 
-python train.py --gpu 1 --model resnet20 --dataset octmnist
-python train.py --gpu 1 --model vgg16 --dataset octmnist
-python train.py --gpu 1 --model convnet --dataset octmnist
+python train.py --gpu 0 --model resnet20 --dataset octmnist
+python train.py --gpu 0 --model vgg16 --dataset octmnist
+python train.py --gpu 0 --model convnet --dataset octmnist
 
-python train.py --gpu 1 --model resnet20 --dataset tissuemnist
-python train.py --gpu 1 --model vgg16 --dataset tissuemnist
-python train.py --gpu 1 --model convnet --dataset tissuemnist
+python train.py --gpu 0 --model resnet20 --dataset tissuemnist
+python train.py --gpu 0 --model vgg16 --dataset tissuemnist
+python train.py --gpu 0 --model convnet --dataset tissuemnist
